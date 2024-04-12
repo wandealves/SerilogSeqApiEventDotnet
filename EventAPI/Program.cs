17cbad276c7389edc3f58903a8865c961542df51
Wanderson Alves <wanderson.alves.rodrigues@gmail.com>
2024-04-11 22:19:16 -0300

    feat: adição do dockerfile

--- EventAPI/Program.cs
+++ EventAPI/Program.cs
@@ -7,11 +7,11 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+//if (app.Environment.IsDevelopment())
+//{
+app.UseSwagger();
+app.UseSwaggerUI();
+//}
 
 app.UseHttpsRedirection();
 
